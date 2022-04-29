d25db44c9eba2513aa2b7387cf143a659f3fde84
Nick Witten <nwitt12@gmail.com>
2022-04-29 01:41:23 -0400

    RTOS controller threading, PID tuning

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -200,11 +200,11 @@ void Mapper::_init_pid(int32_t speed) {
     if (speed == 0) {
         /*              s, mm/s, mm/s, (mm/s)/rad  , (mm/s)/(rad/s),  (mm/s)/(rad*s)   */
         // _pid = new PID(_dt,  800, -800,   800 / M_PI,    20 / M_PI  ,    400 /  M_PI);  // Works well at 0 mm/s
-        _pid = new PID(_dt,  1600, -1600,   10000 / M_PI,    100 / M_PI  ,    50 /  M_PI);  // Works well at 0 mm/s with differential offsets
+        _pid = new PID(_dt,  1500, -1500,   1750 / M_PI,    75 / M_PI  ,    50 /  M_PI);  // Works well at 0 mm/s with differential offsets
     } else if (speed < 100) {
-        _pid = new PID(_dt,  800, -800, 1000 / M_PI,      100 / M_PI,      50 / M_PI);  // Works well at 50 mm/s (works as if at 0 mm/s)
+        _pid = new PID(_dt,  1000, -1000, 1000 / M_PI,      100 / M_PI,      50 / M_PI);  // Works well at 50 mm/s (works as if at 0 mm/s)
     } else {
-        _pid = new PID(_dt,  800, -800, 800 / M_PI,      20 / M_PI,      50 / M_PI);  // Works well at 200 mm/s
+        _pid = new PID(_dt,  1000, -1000, 800 / M_PI,      20 / M_PI,      50 / M_PI);  // Works well at 200 mm/s
     }
 }
 
